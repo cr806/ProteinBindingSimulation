4f4b1aa1800a69f1a2519e987eb936b7fa0a420b
Christopher <christopher.p.reardon@gmail.com>
2022-06-20 11:48:40 +0100

    Added sticky boundary with on and off rate

--- ProteinBinding/Simulation.py
+++ ProteinBinding/Simulation.py
@@ -11,13 +11,14 @@ b_x = 900
 Must be defined in top left quadrant, i.e. x must be negative, y positive
 '''
 boundaries = []
-# boundaries.append(Boundary((-50, -b_y), (50, -b_y), True, 1, 0))
+boundaries.append(Boundary(5, (-50, -b_y), (50, -b_y), True, 1, 0))
 # boundaries.append(Boundary((-50, b_y), (50, b_y), True, 1, 0))
 
 # boundaries.append(Boundary((-b_x, -b_y), (-b_x, b_y), False, 0.5, 0.5))
 # boundaries.append(Boundary((b_x, -b_y), (b_x, b_y), False, 0.5, 0.5))
 # boundaries.append(Boundary((-b_x, -b_y), (-50, -b_y), False, 0.5, 0.5))
-boundaries.append(Boundary(1, (-b_x, -b_y), (b_x, -b_y), False, 0.5, 0.5))
+boundaries.append(Boundary(1, (-b_x, -b_y), (-50, -b_y), False, 0.5, 0.5))
+boundaries.append(Boundary(1, (-50, -b_y), (b_x, -b_y), False, 0.5, 0.5))
 boundaries.append(Boundary(2, (-b_x, b_y), (b_x, b_y), False, 0.5, 0.5))
 boundaries.append(Boundary(3, (b_x, b_y), (b_x, -b_y), False, 0.5, 0.5))
 boundaries.append(Boundary(4, (-b_x, b_y), (-b_x, -b_y), False, 0.5, 0.5))
